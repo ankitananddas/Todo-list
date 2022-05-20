c8f94bb761a4d8a10c41e1d62811b19d6ddf38e3
Ankit Anand Das <ankitanand,das@coditas.com>
2022-05-20 15:18:59 +0530

    third

--- base/static/base/main.js
+++ base/static/base/main.js
@@ -1,22 +1,22 @@
 
 console.log('Hello world!')
 
-const deadline = document.getElementById('deadline')
+const deadline = document.getElementById('deadline');
 
-const date = Date.parse(deadline.textContent)
-console.log(date)
-setInterval(()=>{
-    const now = new Date().getTime()
-    const diff = date - now
-    const day = Math.floor(date / (1000*60*60*24) - (now/(1000*60*60*24)))
-    const hour = Math.floor(date/(1000*60*60) - (now/(1000*60*60)) % 24)
-    const min = Math.floor(date/(1000*60) - (now/(1000*60)) % 60)
-    const second = Math.floor(date/(1000) - (now/(1000))%60)
-    
+const deadlineDate = Date.parse(deadline.textContent);
+
+const myCountDown=setInterval(()=>{
+    const now = new Date().getTime();
+    const diff = deadlineDate - now;
+    const totalSeconds = diff/1000;
+    const day = Math.floor(totalSeconds/ 3600/24);
+    const hour = Math.floor(totalSeconds/3600) % 24;
+    const min = Math.floor(totalSeconds / 60)% 60;
+    const second = Math.floor(totalSeconds % 60);
     if (diff>0) {
-        deadline.innerHTML = day + 'days' + hour + min + second
+        deadline.innerHTML = day + 'days' + hour + "hours" +min +"minutes" + second + "seconds"
     }else {
+        clearInterval(myCountDown)
         deadline.innerHTML = "Deadline Reached"
     }
 },1000)
-
